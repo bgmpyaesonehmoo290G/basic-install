2b4fd49f1212f51ebb590f41edd540f792fd7ce9
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-07-08 10:20:56 +0630

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,32 @@
+#/data/data/com.termux/files/usr/bin/zsh
+
+clear
+echo -e "\e[5m
+      ____ <==> ____
+      \___\(**)/___/
+       \___|  |___/ 
+           L  J 
+           |__|
+            vv\e[0m"
+  echo -e "\e[1;95m Code By H-768\e[0m"
+  echo -e "\em[3;31m basic pkgs install \e[0m"
+  sleep
+  echo -e "\e[5;32m install pkgs plz wait\e[0m"
+  pkg install python -y
+  pkg install git -y
+  pkg install wget -y
+  pkg install php -y
+  pkg install curl -y
+  pkg install perl -y
+  pkg install python3 -y
+  pkg install bash -y
+  pkg install python-dev -y
+  pkg install java -y
+  pkg install nano -y
+  pkg install openssl -y
+  pkg install w3m -y
+  sleep
+  echo -e "\e[5;31m 
+  ######### We are BCKS############"
+  
+        
